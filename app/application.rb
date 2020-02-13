a8afccd6fd32dd6c5b41da116b4a245f74d758c2
Daniel Reina <dannyflatiron@gmail.com>
2020-02-13 05:24:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    binding.pry
 
     if req.path.match(/items/)
-      binding.pry
+      
       # items is used because I found it in the spec folder
       item_price = req.path.split("/items/")
       @@items.find{|s| s.price == item_price}
